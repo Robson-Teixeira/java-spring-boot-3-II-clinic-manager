e6cec4ade1511d1ce34917ce3f5f4a787f497d3d
Robson-Teixeira <robsonmteixeira@outlook.com>
2024-01-06 23:51:46 -0300

    Lidando com erros: Tratando erro 400

--- src/main/java/br/com/alura/clinic/manager/infra/TratadorErros.java
+++ src/main/java/br/com/alura/clinic/manager/infra/TratadorErros.java
@@ -2,8 +2,12 @@ package br.com.alura.clinic.manager.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class TratadorErros {
@@ -11,6 +15,20 @@ public class TratadorErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException methodArgumentNotValidException) {
+        List<FieldError> fieldErrors = methodArgumentNotValidException.getFieldErrors();
+        return ResponseEntity.badRequest().body(fieldErrors.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+
+        public DadosErroValidacao(FieldError fieldError) {
+            this(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
     }
 
 }
