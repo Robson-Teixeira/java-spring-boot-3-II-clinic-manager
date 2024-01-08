fbd77378a9bab579af0d07a2352cb640b781236a
Robson-Teixeira <robsonmteixeira@outlook.com>
2024-01-08 00:09:57 -0300

    JSON Web Token: Outras informações no token

--- src/main/java/br/com/alura/clinic/manager/infra/security/TokenService.java
+++ src/main/java/br/com/alura/clinic/manager/infra/security/TokenService.java
@@ -26,6 +26,7 @@ public class TokenService {
             return JWT.create()
                     .withIssuer("API Voll.med")
                     .withSubject(usuario.getLogin())
+                    .withClaim("id", usuario.getId())
                     .withExpiresAt(dataExpiracao())
                     .sign(algoritmo);
 
