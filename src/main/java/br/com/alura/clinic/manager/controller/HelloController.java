5faa731c93132e042a625fdea0b984f50a00458a
Robson-Teixeira <robsonmteixeira@outlook.com>
2024-01-09 01:46:06 -0300

    Controle de acesso: Controle de acesso por anotações
    @Secured pode ser adicionado nos métodos ou classe (aplica-se em todos os métodos);
    
    @EnableMethodSecurity(securedEnabled = true) é obrigatório para ativar o recurso

--- src/main/java/br/com/alura/clinic/manager/controller/HelloController.java
+++ src/main/java/br/com/alura/clinic/manager/controller/HelloController.java
@@ -1,5 +1,6 @@
 package br.com.alura.clinic.manager.controller;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
+    @Secured("ROLE_ADMIN")
     public String olaMundo() {
         return "Hello World Spring!";
     }
